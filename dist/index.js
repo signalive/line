5700aa35f71fa0cde690ca5e9a3339621ad978fa
Deniz Gurkaynak <dgurkaynak@gmail.com>
2016-11-16 00:22:04 +0300

    chore(*): Forbid direct consumption

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,1 @@
-module.exports = {
-    NodeClient: require('./client-node'),
-    WebClient: require('./client-web'),
-    Server: require('./server')
-};
+throw new Error('Do not consume `line` module directly. Try: `line/server`, `line/client-node` or `line/client-web`');
