986dd0d4f39d81d83bb77ef104bbaf66661ae4bf
Deniz Gurkaynak <dgurkaynak@gmail.com>
2016-11-21 14:11:33 +0300

    docs(*): Kickstart with server

--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,37 @@
 import {Server as WebSocketServer} from 'uws';
 import Connection from './connection';
 import Rooms from './rooms';
-import EventEmitter from 'event-emitter-extra/dist/commonjs.modern';
+import EventEmitterExtra from 'event-emitter-extra/dist/commonjs.modern';
 
 
-class Server extends EventEmitter {
-    constructor(options) {
+/**
+ * Line Server Class
+ * Documentation is here
+ *
+ * @class Server
+ * @extends {EventEmitterExtra}
+ * @param {Object=} options Optional options object.
+ * @param {string=} options.host Server host name. Inherited from uws, [see docs](https://github.com/websockets/ws/blob/master/doc/ws.md#new-websocketserveroptions-callback).
+ * @param {number=} options.port Server port. Inherited from uws, [see docs](https://github.com/websockets/ws/blob/master/doc/ws.md#new-websocketserveroptions-callback).
+ * @param {http.Server=} options.server Server object to be attached. If provided, `host` and `port` will ignored. Inherited from uws, [see docs](https://github.com/websockets/ws/blob/master/doc/ws.md#new-websocketserveroptions-callback).
+ * @param {Function=} options.handleProtocols Inherited from uws, [see docs](https://github.com/websockets/ws/blob/master/doc/ws.md#optionshandleprotocols).
+ * @param {string=} options.path Inherited from uws, [see docs](https://github.com/websockets/ws/blob/master/doc/ws.md#new-websocketserveroptions-callback).
+ * @param {boolean=} options.noServer Inherited from uws, [see docs](https://github.com/websockets/ws/blob/master/doc/ws.md#new-websocketserveroptions-callback).
+ * @param {boolean=} options.clientTracking Inherited from uws, [see docs](https://github.com/websockets/ws/blob/master/doc/ws.md#new-websocketserveroptions-callback).
+ * @param {boolean|Object=} options.perMessageDeflate Inherited from uws, [see docs](https://github.com/websockets/ws/blob/master/doc/ws.md#optionspermessagedeflate).
+ * @param {number=} options.timeout Timeout duration (in ms) for message responses. Default: 30 seconds
+ * @param {number=} options.maxReconnectDelay Maximum reconnection delay (in seconds) for clients. Default: 60 seconds
+ * @param {number=} options.initialReconnectDelay Intial reconnection delay (in seconds) for clients. Defualt: 1 seconds
+ * @param {number=} options.reconnectIncrementFactor Reconnection incremental factor for clients. Default: 2
+ * @param {number=} options.pingInterval Ping interval (in ms) for both server and client. Default: 60 seconds.
+ * @example
+ * const Server = require('line/server');
+ * const server = new Server({
+ *     port: 8080
+ * });
+ */
+class Server extends EventEmitterExtra {
+    constructor(options = {}) {
         super();
 
         this.rooms = new Rooms();
@@ -14,11 +40,26 @@ class Server extends EventEmitter {
             timeout: 30000,
             maxReconnectDelay: 60,
             initialReconnectDelay: 1,
-            reconnectIncrementFactor: 1,
+            reconnectIncrementFactor: 2,
             pingInterval: 60000
-        }, options || {});
+        }, options);
     }
 
+    /**
+     * Starts the server.
+     *
+     * @returns {Promise}
+     * @memberOf Server
+     * @example
+     * server
+     *     .start()
+     *     .then(() => {
+     *         console.log('Server started');
+     *     })
+     *     .catch((err) => {
+     *         console.log('Server could not started', err);
+     *     });
+     */
     start() {
         if (!this.options.port) {
             this.server = new WebSocketServer(this.options);
@@ -36,6 +77,21 @@ class Server extends EventEmitter {
     }
 
 
+    /**
+     * Stops the server.
+     *
+     * @returns {Promise}
+     * @memberOf Server
+     * @example
+     * server
+     *     .stop()
+     *     .then(() => {
+     *         console.log('Server stopped');
+     *     })
+     *     .catch((err) => {
+     *         console.log('Server could not stopped', err);
+     *     });
+     */
     stop() {
         if (!this.server) {
             const err = new Error('Could not stop server. Server is probably not started, or already stopped.');
@@ -73,17 +129,65 @@ class Server extends EventEmitter {
     }
 
 
+    /**
+     * Gets a connection by id
+     *
+     * @param {string} id
+     * @returns {?Connection}
+     * @memberOf Server
+     * @example
+     * const connection = server.getConnectionById('someId');
+     *
+     * if (connection)
+     *     connection.send('hello', {world: ''});
+     */
     getConnectionById(id) {
         return this.rooms.getRoom('/').getConnectionById(id);
     }
 
 
+    /**
+     * Broadcasts a message to all the connected clients.
+     *
+     * @param {string} eventName
+     * @param {any} payload
+     * @memberOf Server
+     * @example
+     * server.broadcast('hello', {world: ''});
+     */
     broadcast(eventName, payload) {
-        return this.rooms.getRoom('/').broadcast(eventName, payload);
+        this.rooms.getRoom('/').broadcast(eventName, payload);
     }
 }
 
 
+/**
+ * `Server` is an [full-fledged event emitter](https://github.com/signalive/event-emitter-extra).
+ *
+ * @param {'connection'} Server.Events.CONNECTION
+ * @param {'handshake'} Server.Events.HANDSHAKE
+ * @param {'headers'} Server.Events.HEADERS
+ * @param {'error'} Server.Events.ERROR
+ * @example
+ * // On a client connects after successful handshake
+ * server.on('connection', (connection) => {
+ *    connection.send('hello');
+ *    ...
+ * });
+ *
+ * // If you want to authorize your client
+ * server.on('handshake', (connection, handshake) => {
+ *    if (handshake.payload.token == 'test')
+ *        handshake.resolve();
+ *    else
+ *        handshake.reject(new Error('Invalid token'));
+ * });
+ *
+ * // Catch errors
+ * server.on('error', (err) => {
+ *    ...
+ * });
+ */
 Server.Events = {
     CONNECTION: 'connection',
     HANDSHAKE: 'handshake',
