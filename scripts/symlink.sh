065758f6ce39af11a603101d4c758608de16f78d
Deniz Gurkaynak <dgurkaynak@gmail.com>
2016-11-29 00:25:04 +0300

    chore(*): Fix typescript client declarations

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -2,8 +2,16 @@ if [ ! -f client-node.js ]; then
     ln -s "./dist/client-node.js" client-node.js
 fi
 
+if [ ! -f client-node.d.ts ]; then
+    ln -s "./src/client/client-node.d.ts" client-node.d.ts
+fi
+
 if [ ! -f client-web.js ]; then
     ln -s "./dist/client-web.js" client-web.js
+fi
+
+if [ ! -f client-web.d.ts ]; then
+    ln -s "./src/client/client-web.d.ts" client-web.d.ts
 fi
 
 if [ ! -f server.js ]; then
---
+++ src/client/client-web.d.ts
@@ -0,0 +1,55 @@
+declare module 'line-socket/client-web' {
+    import EventEmitterExtra = require('event-emitter-extra');
+
+    type LineClientState = -1 | 0 | 1 | 2 | 3;
+    type LineClientEvent = '_connecting' | '_connecting_error' | '_connected' | '_closed' | '_error';
+
+    class LineClient extends EventEmitterExtra {
+        static Events: {
+            CONNECTING: LineClientEvent,
+            CONNECTING_ERROR: LineClientEvent,
+            CONNECTED: LineClientEvent,
+            CLOSED: LineClientEvent,
+            ERROR: LineClientEvent
+        };
+
+        static States: {
+            READY: LineClientState,
+            CONNECTING: LineClientState,
+            CONNECTED: LineClientState,
+            CLOSING: LineClientState,
+            CLOSED: LineClientState
+        };
+
+        id: string;
+        url: string;
+        state: LineClientState;
+
+        constructor(url: string, options?: {
+            reconnect?: boolean,
+            handshakePayload?: any
+        });
+
+        connect(): Promise<null>;
+
+        disconnect(
+            code?: number,
+            reason?: any,
+            retry?: boolean
+        ): Promise<null>;
+
+        ping(): Promise<null>;
+
+        send(
+            eventName: string,
+            payload?: any
+        ): Promise<any>;
+
+        sendWithoutResponse(
+            eventName: string,
+            payload?: any
+        ): Promise<null>;
+    }
+
+    export = LineClient;
+}
