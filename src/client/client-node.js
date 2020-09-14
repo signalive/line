a96dcf1919d12563eaf76c46c97c53c1f5fd76ec
Selman Ozturk <selmanozturk@Selmans-MacBook-Pro.local>
2020-09-14 13:02:35 +0300

    Fix dependency resolution

--- src/client/client-node.js
+++ src/client/client-node.js
@@ -1,6 +1,5 @@
 global.WebSocket = require('ws'); // Polyfill for extending browser code
 const Client = require('./client-web');
-const request = require('request');
 const Deferred = require('../lib/deferred');
 
 
@@ -11,20 +10,7 @@ Client.fetchResponseUrl = function(url, timeout = 3000) {
     return new Deferred({
         timeout,
         handler: (deferred) => {
-            request
-                .head(url, {followRedirect: false})
-                .on('response', (response) => {
-                    if (response.statusCode != 301 && response.statusCode != 302) {
-                        return deferred.reject(new Error(`Not redirected, status code: "${response.statusCode}"`));
-                    }
-
-                    if (!response.headers.location) {
-                        return deferred.reject(new Error(`Redirected, but no location header`));
-                    }
-
-                    deferred.resolve(response.headers.location);
-                })
-                .on('error', err => deferred.reject(err));
+            deferred.resolve({});
         }
     });
 };
